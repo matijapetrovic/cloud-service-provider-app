96f86fed40b78f486ea678031c48730383a77dc5
Nikola Kabasaj <nikolakabasaj6@gmail.com>
2019-12-10 15:32:52 +0100

    Index controller started

--- src/controllers/IndexController.java
+++ src/controllers/IndexController.java
@@ -1,11 +1,16 @@
 package controllers;
 
+import javax.websocket.Session;
+
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class IndexController {
 	public static Route serveIndexPage = (Request request, Response response) -> {
-		return "";
+		if(request.session().attribute("user") == null)
+			return false;
+		
+		return "Hello from index page";
 	};
 }
