c761037fac4b5e4ea12bc6021afaab7145561e2b
Nikola Kabasaj <nikolakabasaj6@gmail.com>
2019-12-10 15:55:10 +0100

    IndexController fixed

--- src/controllers/IndexController.java
+++ src/controllers/IndexController.java
@@ -6,9 +6,11 @@ import spark.Route;
 
 public class IndexController {
 	public static Route serveIndexPage = (Request request, Response response) -> {
-		if(request.session().attribute("user") == null)
+		if(request.session().attribute("user") == null){
+			request.session().attribute("loginRedirect", true);
 			response.redirect("/login");
+		}
 		
 		return "Hello , "+ request.session().attribute("user");
-	};
+	};	
 }
