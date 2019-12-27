f5658dba252faae7e414659e72f615d2d61573f5
Matija Petrovic <matijapetrovicweb@gmail.com>
2019-12-27 13:03:39 +0100

    Changed the way user service is used for organizations

--- spark-api/src/serializer/organization/OrganizationSerializer.java
+++ spark-api/src/serializer/organization/OrganizationSerializer.java
@@ -48,7 +48,7 @@ public class OrganizationSerializer extends JSONSerializer<Organization> {
         List<User> users = new ArrayList<User>();
         org.getUsers()
                 .forEach(x -> App.userService
-                        .getUser(x.getEmail())
+                        .findByKey(x.getKey())
                         .ifPresent(users::add));
         org.setUsers(users);
     }
