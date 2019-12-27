65796c441e748fd2ba503216fa3eb6d5dadce442
Nikola Kabasaj <nikolakabasaj6@gmail.com>
2019-12-27 12:45:49 +0100

    Commit changes second time for user service

--- vue-app/src/app/user/users.js
+++ vue-app/src/app/user/users.js
@@ -1,28 +1,9 @@
 Vue.component("users-page",{
     template: `
 
-    <div> 
-        <super-admin-page></super-admin-page>
-        <!-- <admin-page></admin-page>-->     
-    </div>
-
-    <base-layout v-bind:page-title="$route.meta.title">
-        <table border="1" class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th>Email</th>
-                    <th>Name</th>
-                    <th>Surname</th>
-                    <th>Organization</th>
-                </tr>
-            </thead>
-            <tr v-for="user in users">
-                <td><a href="#">{{ user.email }}</a></td>
-                <td>{{ user.name }}</td>
-                <td>{{ user.surname }}</td>
-                <td>{{ user.organization }}</td>
-            </tr>
-        </table>
+        <base-layout v-bind:page-title="$route.meta.title">
+            <super-admin-page></super-admin-page>
+        <!-- <admin-page></admin-page>-->    
     </base-layout>
     `
     ,
